9dd427ac96b11ea47b70accd28a0df7d476c7af8
Aaron Scruggs <aarondscruggs@gmail.com>
2017-01-20 17:11:47 -0800

    Can search fighter by name in admin

--- fights/admin.py
+++ fights/admin.py
@@ -8,7 +8,7 @@ class FighterAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "birthday", "height", "weight", "nickname",
                     "location", "country", "camp", "sherdog_url")
 
-    search_fields = ["sherdog_url"]
+    search_fields = ["sherdog_url", 'name']
 
 
 @admin.register(Fight)
