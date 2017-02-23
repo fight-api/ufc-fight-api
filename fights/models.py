9670b494e4deec9b0ad7b65ca770fe896ced16dc
Aaron Scruggs <aarondscruggs@gmail.com>
2017-02-22 17:35:19 -0800

    Add fighter streak fields

--- fights/models.py
+++ fights/models.py
@@ -82,6 +82,7 @@ class Fight(models.Model):
     winner_url = models.CharField(max_length=255)
     winner_experience = models.IntegerField(null=True, blank=True)
     winner_age = models.FloatField(null=True, blank=True)
+    winner_streak = models.IntegerField(null=True, blank=True)
 
     loser = models.ForeignKey(Fighter, null=True, blank=True,
                               related_name='losers')
@@ -89,6 +90,7 @@ class Fight(models.Model):
     loser_url = models.CharField(max_length=255)
     loser_experience = models.IntegerField(null=True, blank=True)
     loser_age = models.FloatField(null=True, blank=True)
+    loser_streak = models.IntegerField(null=True, blank=True)
 
     event = models.ForeignKey(Event, null=True, blank=True)
     method = models.CharField(max_length=255)
