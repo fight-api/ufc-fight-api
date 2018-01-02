c6f145ec6d36b176b054b621dd74721864494cce
Aaron Scruggs <aarondscruggs@gmail.com>
2018-01-02 13:03:34 -0800

    Add several Fight model tests

--- fights/tests.py
+++ fights/tests.py
@@ -123,3 +123,133 @@ class FighterModelTests(TestCase):
     def test_finish_rate(self):
         self.assertEqual(self.fighter1.finish_rate, 2/3)
         self.assertEqual(self.fighter2.finish_rate, 2/3)
+
+
+class FightModelTests(TestCase):
+    def setUp(self):
+        self.fighter1 = Fighter.objects.create(
+            name='f1',
+            dt_birthday=datetime.datetime(1990, 1, 1),
+            birthday='January 1st, 1990',
+            height="5' 10",
+            weight='170 lbs',
+            sh_url='test.com/1'
+        )
+        self.fighter2 = Fighter.objects.create(
+            name='f2',
+            dt_birthday=datetime.datetime(1992, 6, 6),
+            birthday='June 6th, 1992',
+            height="5' 11",
+            weight='170 lbs',
+            sh_url='test.com/2'
+        )
+        self.event1 = Event.objects.create(
+            title='event1',
+            organization='org',
+            date_string='Feb 2 2016',
+            dt_date=datetime.datetime(2016, 2, 1),
+            location='Las Vegas'
+        )
+        self.event2 = Event.objects.create(
+            title='event2',
+            organization='org',
+            date_string='March 2 2016',
+            dt_date=datetime.datetime(2016, 3, 1),
+            location='Las Vegas'
+        )
+        self.event3 = Event.objects.create(
+            title='event3',
+            organization='org',
+            date_string='April 2 2016',
+            dt_date=datetime.datetime(2016, 4, 1),
+            location='Las Vegas'
+        )
+        self.fight1 = Fight.objects.create(
+            winner=self.fighter1,
+            winner_name=self.fighter1.name,
+            winner_url=self.fighter1.sh_url,
+            loser=self.fighter2,
+            loser_name=self.fighter2.name,
+            loser_url=self.fighter2.sh_url,
+            event=self.event1,
+            method='technical ko',
+            referee='Herb Dean',
+            round='1',
+            time='2.53'
+        )
+        self.fight2 = Fight.objects.create(
+            winner=self.fighter1,
+            winner_name=self.fighter1.name,
+            winner_url=self.fighter1.sh_url,
+            loser=self.fighter2,
+            loser_name=self.fighter2.name,
+            loser_url=self.fighter2.sh_url,
+            event=self.event2,
+            method='split decision',
+            referee='Herb Dean',
+            round='3',
+            time='5.00'
+        )
+        self.fight3 = Fight.objects.create(
+            winner=self.fighter2,
+            winner_name=self.fighter2.name,
+            winner_url=self.fighter2.sh_url,
+            loser=self.fighter1,
+            loser_name=self.fighter1.name,
+            loser_url=self.fighter1.sh_url,
+            event=self.event3,
+            method='submission',
+            referee='Herb Dean',
+            round='2',
+            time='2.53'
+        )
+
+    def test_get_finish_type(self):
+        self.assertEqual(self.fight1.get_finish_type(), 'ko')
+        self.assertEqual(self.fight2.get_finish_type(), 'decision')
+        self.assertEqual(self.fight3.get_finish_type(), 'submission')
+
+    def test_set_fighter_ages(self):
+        self.assertEqual(self.fight1.winner_age, 26.101369863013698)
+        self.assertEqual(self.fight1.winner_int_age, 26)
+        self.assertEqual(self.fight1.loser_age, 23.671232876712327)
+        self.assertEqual(self.fight1.loser_int_age, 23)
+
+        # Assign new birthday to fighters and re-set ages
+        self.fighter1.dt_birthday = datetime.datetime(1991, 1, 1)
+        self.fighter2.dt_birthday = datetime.datetime(1988, 6, 6)
+        self.fight1.set_fighter_ages()
+
+        self.assertAlmostEqual(self.fight1.winner_age, 25.101369863013698, 2)
+        self.assertEqual(self.fight1.winner_int_age, 25)
+        self.assertAlmostEqual(self.fight1.loser_age, 27.671232876712327, 2)
+        self.assertEqual(self.fight1.loser_int_age, 27)
+
+    def test_get_streak(self):
+
+        self.assertEqual(self.fight1.get_streak(self.fighter1), 0)
+        self.assertEqual(self.fight1.get_streak(self.fighter2), 0)
+        self.assertEqual(self.fight2.get_streak(self.fighter1), 1)
+        self.assertEqual(self.fight2.get_streak(self.fighter2), -1)
+        self.assertEqual(self.fight3.get_streak(self.fighter1), 2)
+        self.assertEqual(self.fight3.get_streak(self.fighter2), -2)
+
+
+    def test_set_fighter_streaks(self):
+        pass
+
+    def test_calc_stats(self):
+        # Set new values to get different results from initial save
+        pass
+
+    def test_str(self):
+        pass
+
+    def test_unique_together(self):
+        pass
+
+
+
+
+class FightQueryModelTests(TestCase):
+    pass
